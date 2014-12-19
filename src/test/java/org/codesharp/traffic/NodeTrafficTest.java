eccd515afb61199dd126cdeacc6f65a212f8348e
houkun <houkun@taobao.com>
2014-12-19 23:14:58 +0800

    make NodeTrafficTest reuseble

--- src/test/java/org/codesharp/traffic/NodeTrafficTest.java
+++ src/test/java/org/codesharp/traffic/NodeTrafficTest.java
@@ -10,15 +10,16 @@ import org.junit.After;
 import org.junit.Test;
 
 public class NodeTrafficTest {
-	private Object n1_flag = "N1";
-	private Object n2_flag = "N2";
-	private Object n3_flag = "N3";
-	private Object n4_flag = "N4";
-	private Object n2_n1_id = "2-1";
-	private Object n2_n3_id = "2-3";
-	private Object n3_n4_id = "3-4";
+	protected Object n1_flag = "N1";
+	protected Object n2_flag = "N2";
+	protected Object n3_flag = "N3";
+	protected Object n4_flag = "N4";
+	protected Object n2_n1_id = "2-1";
+	protected Object n2_n3_id = "2-3";
+	protected Object n2_n4_id = "2-4";
+	protected Object n3_n4_id = "3-4";
 	
-	private Queue<Object> path = new LinkedList<Object>();
+	protected Queue<Object> path = new LinkedList<Object>();
 	
 	@After
 	public void before() {
@@ -26,33 +27,28 @@ public class NodeTrafficTest {
 	}
 	
 	@Test
-	public void N1_N2_N3_traffic_test() {
-		MessageHandle handle = new SimpleMessageHandle();
-		Message msg = new Message();
-		msg.Destination = n3_flag;
+	public void N1_N2_N4_traffic_test() {
+		MessageHandle handle = newHandle();
+		Object msg = newMessage(n4_flag);
 		
 		Node n2 = newNode(handle, n2_flag);
 		// n1,n2 connect
 		Connection n1_n2 = newConnection(n2_n1_id, n2);
-		NodeProxy n1 = newProxy(n1_flag);
-		n2.accept(n1, n1_n2);
-		// n2,n3 connect
-		Connection n2_n3 = newConnection(n2_n3_id, n2);
-		NodeProxy n3 = newProxy(n3_flag);
-		n2.accept(n3, n2_n3);
+		n2.accept(newProxy(n1_flag), n1_n2);
+		// n2,n4 connect
+		Connection n2_n4 = newConnection(n2_n4_id, n2);
+		n2.accept(newProxy(n4_flag), n2_n4);
 		
 		n1_n2.onMessage(msg);
-		msg.Command = Commands.ACK;
-		n2_n3.onMessage(msg);
+		n2_n4.onMessage(newAck(msg));
 		
-		assertPath(n2_n1_id, n2_flag, n2_n3_id, n2_flag);
+		assertPath(n2_n1_id, n2_flag, n2_n4_id, n2_flag);
 	}
 	
 	@Test
 	public void N1_N2_N3_N4_traffic_test() {
-		MessageHandle handle = new SimpleMessageHandle();
-		Message msg = new Message();
-		msg.Destination = n4_flag;
+		MessageHandle handle = newHandle();
+		Object msg = newMessage(n4_flag);
 		
 		Node n2 = newNode(handle, n2_flag);
 		Node n3 = newNode(handle, n3_flag);
@@ -70,13 +66,27 @@ public class NodeTrafficTest {
 		n3.accept(newProxy(n2_flag), n3_n2);
 		
 		n1_n2.onMessage(msg);
-		msg.Command = Commands.ACK;
-		n3_n4.onMessage(msg);
+		n3_n4.onMessage(newAck(msg));
 		
 		assertPath(n2_n1_id, n2_flag, n2_n3_id, n3_flag, n3_n4_id, n3_flag, n2_n3_id, n2_flag);
 	}
 	
-	private Node newNode(MessageHandle handle, final Object flag) {
+	protected MessageHandle newHandle() {
+		return new SimpleMessageHandle();
+	}
+	
+	protected Object newMessage(Object dst) {
+		Message msg = new Message();
+		msg.Destination = dst;
+		return msg;
+	}
+	
+	protected Object newAck(Object msg) {
+		((Message) msg).Command = Commands.ACK;
+		return msg;
+	}
+	
+	protected Node newNode(MessageHandle handle, final Object flag) {
 		return new Node(handle) {
 			@Override
 			protected void process(Object msg) {
@@ -96,11 +106,11 @@ public class NodeTrafficTest {
 		};
 	}
 	
-	private NodeProxy newProxy(final Object flag) {
+	protected NodeProxy newProxy(final Object flag) {
 		return newProxy(flag, null, null);
 	}
 	
-	private NodeProxy newProxy(final Object flag, final Object at, final Connection conn) {
+	protected NodeProxy newProxy(final Object flag, final Object at, final Connection conn) {
 		return new NodeProxy() {
 			@Override
 			public void send(Object msg) {
@@ -115,11 +125,11 @@ public class NodeTrafficTest {
 		};
 	}
 	
-	private Connection newConnection(final Object id, final Node local) {
+	protected Connection newConnection(final Object id, final Node local) {
 		return newConnection(id, local, null);
 	}
 	
-	private Connection newConnection(final Object id, final Node local, final Connection remote) {
+	protected Connection newConnection(final Object id, final Node local, final Connection remote) {
 		return new Connection(local) {
 			@Override
 			public void send(Object msg) {
