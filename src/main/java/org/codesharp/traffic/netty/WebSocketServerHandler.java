393ab5675cfcdc481f26c6bc8f1daee8c30c0ef4
houkun <houkun@taobao.com>
2015-01-15 17:04:01 +0800

    #1;impl WebSocketServerHandler and server test

---
+++ src/main/java/org/codesharp/traffic/netty/WebSocketServerHandler.java
@@ -0,0 +1,127 @@
+package org.codesharp.traffic.netty;
+
+import org.codesharp.traffic.drpc.Frontend;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
+import io.netty.util.CharsetUtil;
+import static io.netty.handler.codec.http.HttpMethod.*;
+import static io.netty.handler.codec.http.HttpResponseStatus.*;
+import static io.netty.handler.codec.http.HttpVersion.*;
+
+public abstract class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+	private WebSocketServerHandshaker handshaker;
+	private Frontend frontend;
+	
+	protected abstract Frontend newFrontend(ChannelHandlerContext ctx, HttpRequest req) throws Exception;
+	
+	@Override
+	public void channelRead0(ChannelHandlerContext ctx, Object msg) {
+		if (msg instanceof FullHttpRequest) {
+			handleHttpRequest(ctx, (FullHttpRequest) msg);
+		} else if (msg instanceof WebSocketFrame) {
+			handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+		}
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+	
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		ctx.flush();
+	}
+	
+	private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+		// Handle a bad request.
+		if (!req.decoderResult().isSuccess()) {
+			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, BAD_REQUEST));
+			return;
+		}
+		
+		// Allow only GET methods.
+		if (req.method() != GET) {
+			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, FORBIDDEN));
+			return;
+		}
+		
+		// Handshake
+		WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(req.uri(), null, false);
+		handshaker = wsFactory.newHandshaker(req);
+		if (handshaker == null) {
+			WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
+			return;
+		}
+		
+		try {
+			this.frontend = this.newFrontend(ctx, req);
+			handshaker.handshake(ctx.channel(), req);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, UNAUTHORIZED));
+		}
+	}
+	
+	private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+		if (frame instanceof CloseWebSocketFrame) {
+			handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+			return;
+		}
+		if (frame instanceof PingWebSocketFrame) {
+			ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+			return;
+		}
+		if (!(frame instanceof TextWebSocketFrame)) {
+			throw new UnsupportedOperationException(String.format("%s frame types not supported", frame.getClass()
+					.getName()));
+		}
+		
+		try {
+			this.frontend.onMessage(((TextWebSocketFrame) frame).text());
+		} catch (Exception e) {
+			e.printStackTrace();
+			handshaker.close(ctx.channel(),
+					new CloseWebSocketFrame(true, 0,
+							frame.content().clear()
+									.writeShort(1000)
+									.writeBytes(e.getMessage().getBytes(CharsetUtil.UTF_8))
+									.retain()));
+		}
+	}
+	
+	private static void sendHttpResponse(
+			ChannelHandlerContext ctx, FullHttpRequest req, FullHttpResponse res) {
+		// Generate an error page if response getStatus code is not OK (200).
+		if (res.status().code() != 200) {
+			ByteBuf buf = Unpooled.copiedBuffer(res.status().toString(), CharsetUtil.UTF_8);
+			res.content().writeBytes(buf);
+			buf.release();
+			HttpHeaders.setContentLength(res, res.content().readableBytes());
+		}
+		
+		// Send the response and close the connection if necessary.
+		ChannelFuture f = ctx.channel().writeAndFlush(res);
+		if (!HttpHeaders.isKeepAlive(req) || res.status().code() != 200) {
+			f.addListener(ChannelFutureListener.CLOSE);
+		}
+	}
+}
---
+++ src/test/java/org/codesharp/traffic/netty/WebSocketServerTest.java
@@ -0,0 +1,104 @@
+package org.codesharp.traffic.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import org.codesharp.traffic.Asserter;
+import org.codesharp.traffic.Node;
+import org.codesharp.traffic.NodeProxy;
+import org.codesharp.traffic.drpc.Frontend;
+import org.junit.Test;
+
+public class WebSocketServerTest {
+	DRPCMessageHandleImpl handle = new DRPCMessageHandleImpl(ByteBufAllocator.DEFAULT);
+	Node node = new Node(handle) {
+		@Override
+		protected void process(Object msg) {
+		}
+		
+		@Override
+		public Object flag() {
+			return null;
+		}
+	};
+	
+	@Test
+	public void run_test() throws InterruptedException {
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.handler(new LoggingHandler(LogLevel.INFO))
+					.childHandler(new WebSocketServerInitializer());
+			
+			Channel ch = b.bind(8080).sync().channel();
+			
+			ch.closeFuture().sync();
+		} finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+	
+	public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel> {
+		@Override
+		public void initChannel(SocketChannel ch) throws Exception {
+			ChannelPipeline pipeline = ch.pipeline();
+			pipeline.addLast(new HttpServerCodec());
+			pipeline.addLast(new HttpObjectAggregator(65536));
+			pipeline.addLast(new WebSocketServerHandler() {
+				@Override
+				protected Frontend newFrontend(
+						final ChannelHandlerContext ctx, HttpRequest req)
+						throws Exception {
+					
+					final long idAndFlag = System.currentTimeMillis();
+					System.out.println(idAndFlag);
+					
+					Frontend frontend = new Frontend(node, handle) {
+						@Override
+						public Object id() {
+							return idAndFlag;
+						}
+						
+						@Override
+						protected void internalSend(Object msg) {
+							ctx.channel().write(new TextWebSocketFrame((ByteBuf) msg));
+						}
+					};
+					
+					node.accept(new NodeProxy() {
+						@Override
+						public void send(Object msg) {
+							Asserter.throwUnsupported(null);
+						}
+						
+						@Override
+						public Object flag() {
+							return idAndFlag;
+						}
+					}, frontend);
+					
+					return frontend;
+				}
+			});
+		}
+	}
+}
